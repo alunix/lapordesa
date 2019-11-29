862e87b21a508f4b22e76e5b8019ed4753a1623a
Veris Juniardi <admin@si-desa.id>
2019-11-29 12:04:55 +0700

    update

---
+++ lib/screen/profile/profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:async';
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screen/welcome/login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
